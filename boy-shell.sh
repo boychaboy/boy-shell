d6ec3d26eb3ed6026b528232a2c2a60166a7ec0b
boychaboy <hoon2j@gmail.com>
2020-07-03 21:49:24 +0900

    shell file added

---
+++ boy-shell.sh
@@ -0,0 +1,14 @@
+sudo apt-get install zsh
+sudo chsh -s /usr/bin/zsh
+echo $SHELL
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo apt install neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# neovim/python-client
+pip2 install pynvim
+pip3 install pynvim
+
