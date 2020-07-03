3a52dad3dd418d2b6f657c74e83aa6e4271d923c
boychaboy <hoon2j@gmail.com>
2020-07-03 23:13:19 +0900

    cp modified

--- boy-shell.sh
+++ boy-shell.sh
@@ -6,8 +6,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-cp .zshrc ../
-cp -r .config/ ../
+sudo cp boy-shell/.zshrc .
+sudo cp -r boy-shell/.config/ .
 source .zshrc
 
 # brew
