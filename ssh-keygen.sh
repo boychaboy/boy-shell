8b200312e00f9c48d1fdf2d33a0bef2500b51d95
Hawaii Mac <hawaii@Hawaiis-MacBook-Pro.local>
2020-07-04 17:16:51 +0900

    ssh-keygen added

---
+++ ssh-keygen.sh
@@ -0,0 +1,11 @@
+ssh-keygen -f ~/.ssh/id_rsa_224
+ssh-keygen -f ~/.ssh/id_rsa_228
+ssh-keygen -f ~/.ssh/id_rsa_229
+ssh-keygen -f ~/.ssh/id_rsa_230
+ssh-keygen -f ~/.ssh/id_rsa_231
+
+ssh-copy-id -i ~/.ssh/id_rsa_224.pub boychaboy@163.239.199.224
+ssh-copy-id -i ~/.ssh/id_rsa_228.pub boychaboy@163.239.199.228
+ssh-copy-id -i ~/.ssh/id_rsa_229.pub boychaboy@163.239.199.229
+ssh-copy-id -i ~/.ssh/id_rsa_230.pub boychaboy@163.239.199.230
+ssh-copy-id -i ~/.ssh/id_rsa_231.pub boychaboy@163.239.199.231
